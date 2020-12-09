353548597591e764b4e108b4ad177699a5cc17ae
marko adzaga <marko.4adzaga.4@gmail.com>
2020-12-09 11:33:29 +0100

    Added Menu API

---
+++ web/Controllers/Api/MenuApiController.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using web.Data;
+using web.Models;
+
+namespace web.Controllers_Api
+{
+    [Route("api/v1/Menu")]
+    [ApiController]
+    public class MenuApiController : ControllerBase
+    {
+        private readonly MaoContext _context;
+
+        public MenuApiController(MaoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MenuApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Menu>>> GetMenus()
+        {
+            return await _context.Menus.ToListAsync();
+        }
+
+        // GET: api/MenuApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Menu>> GetMenu(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return menu;
+        }
+
+        // PUT: api/MenuApi/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMenu(int id, Menu menu)
+        {
+            if (id != menu.MenuID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(menu).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MenuExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MenuApi
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Menu>> PostMenu(Menu menu)
+        {
+            _context.Menus.Add(menu);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMenu", new { id = menu.MenuID }, menu);
+        }
+
+        // DELETE: api/MenuApi/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Menu>> DeleteMenu(int id)
+        {
+            var menu = await _context.Menus.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            _context.Menus.Remove(menu);
+            await _context.SaveChangesAsync();
+
+            return menu;
+        }
+
+        private bool MenuExists(int id)
+        {
+            return _context.Menus.Any(e => e.MenuID == id);
+        }
+    }
+}
