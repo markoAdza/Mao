3d3bdbd4259f1bc0648d15e2999bac9feee5244f
marko adzaga <marko.4adzaga.4@gmail.com>
2020-12-18 17:38:26 +0100

    minor

--- web/Startup.cs
+++ web/Startup.cs
@@ -47,7 +47,7 @@ namespace web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You  may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
